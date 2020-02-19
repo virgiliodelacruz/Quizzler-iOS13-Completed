1dd355894473663b346051cfde3ebbfa0682f397
Mtro. Virgilio De la Cruz <virgil85@gmail.com>
2020-02-18 19:27:23 -0600

    modifique 2 pregunta

--- Quizzler-iOS13/Model/QuizBrain.swift
+++ Quizzler-iOS13/Model/QuizBrain.swift
@@ -15,7 +15,7 @@ struct QuizBrain {
     
     let quiz = [
         Question(q: "Maradona fue campeón en México 86", a: "True"),
-        Question(q: "Approximately one quarter of human bones are in the feet.", a: "True"),
+        Question(q: "México fue campeón en el mundial 94", a: "False"),
         Question(q: "The total surface area of two human lungs is approximately 70 square metres.", a: "True"),
         Question(q: "In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.", a: "True"),
         Question(q: "In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.", a: "False"),
